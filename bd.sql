a962d86896c5ec7ba91470d32a9ce9f05bb2beee
vinnvi <vin.doisneau@free.fr>
2015-12-23 16:45:23 +0100

    -ajout des matieres des 4 dut de velizy (/!\ les matieres ne sont pas encore reliees a leurs disciplines)

--- bd.sql
+++ bd.sql
@@ -70,33 +70,64 @@ INSERT INTO groupe (filiere,annee) VALUES ('INFO',2);
 INSERT INTO groupe (filiere,annee) VALUES ('MMI',2);
 INSERT INTO groupe (filiere,annee) VALUES ('RT',2);
 
-INSERT INTO matiere (nom_mat) VALUES ('Anglais');
-INSERT INTO matiere (nom_mat) VALUES ('Algorithmique');
-INSERT INTO matiere (nom_mat) VALUES ('Architecture des systèmes informatiques');
-INSERT INTO matiere (nom_mat) VALUES ('Bases de données');
-INSERT INTO matiere (nom_mat) VALUES ('Conception orientée objet');
-INSERT INTO matiere (nom_mat) VALUES ('Conception de documents');
-INSERT INTO matiere (nom_mat) VALUES ('Systèmes informatique');
-INSERT INTO matiere (nom_mat) VALUES ('Mathématiques discrètes');
-INSERT INTO matiere (nom_mat) VALUES ('Algèbre linéaire');
-INSERT INTO matiere (nom_mat) VALUES ('Expression-communication');
-INSERT INTO matiere (nom_mat) VALUES ('Architecture réseaux');
-INSERT INTO matiere (nom_mat) VALUES ('Programmation orientée objet');
-INSERT INTO matiere (nom_mat) VALUES ('Graphes et langages');
-INSERT INTO matiere (nom_mat) VALUES ('Gestion de projet');
-INSERT INTO matiere (nom_mat) VALUES ('Programmation web');
-INSERT INTO matiere (nom_mat) VALUES ('Services réseaux');
-INSERT INTO matiere (nom_mat) VALUES ('Probabilités et statistiques');
-INSERT INTO matiere (nom_mat) VALUES ('Modélisations mathématiques');
-INSERT INTO matiere (nom_mat) VALUES ('Droit des TIC');
-INSERT INTO matiere (nom_mat) VALUES ('Gestion des systèmes d informations');
-INSERT INTO matiere (nom_mat) VALUES ('Systèmes d exploitation');
-INSERT INTO matiere (nom_mat) VALUES ('Comptabilité');
-INSERT INTO matiere (nom_mat) VALUES ('IHM');
-INSERT INTO matiere (nom_mat) VALUES ('Economie');
-INSERT INTO matiere (nom_mat) VALUES ('Fonctionnement des organisations');
-INSERT INTO matiere (nom_mat) VALUES ('Conception et développement mobile');
-INSERT INTO matiere (nom_mat) VALUES ('Programmation répartie');
-INSERT INTO matiere (nom_mat) VALUES ('Administration système et réseau');
 
+--Matieres générales
+INSERT INTO matiere (nom_mat) VALUES ('Anglais');
+INSERT INTO matiere (nom_mat) VALUES ('Physique');
+INSERT INTO matiere (nom_mat) VALUES ('Expression,Communication');
+INSERT INTO matiere (nom_mat) VALUES ('PPP');
+INSERT INTO matiere (nom_mat) VALUES ('Mathematiques');
+INSERT INTO matiere (nom_mat) VALUES ('Economie');
+INSERT INTO matiere (nom_mat) VALUES ('Gestion, Gestion projet');
+INSERT INTO matiere (nom_mat) VALUES ('Organisation');
+INSERT INTO matiere (nom_mat) VALUES ('Droit');
+INSERT INTO matiere (nom_mat) VALUES ('Expression,Communication');
+
+--Matieres Specifiques a l'informatique
+INSERT INTO matiere (nom_mat) VALUES ('Algorithmique');
+INSERT INTO matiere (nom_mat) VALUES ('Programmation');
+INSERT INTO matiere (nom_mat) VALUES ('Architecture Materielle');
+INSERT INTO matiere (nom_mat) VALUES ('Systèmes exploitation');
+INSERT INTO matiere (nom_mat) VALUES ('Reseaux');
+INSERT INTO matiere (nom_mat) VALUES ('Web');
+INSERT INTO matiere (nom_mat) VALUES ('Internet');
+INSERT INTO matiere (nom_mat) VALUES ('Mobilité');
+INSERT INTO matiere (nom_mat) VALUES ('Bases de donnees');
+INSERT INTO matiere (nom_mat) VALUES ('Analyse/conception et développement d applis');
+
+--Matieres Specifiques aux GEII
+INSERT INTO matiere (nom_mat) VALUES ('Energie');
+INSERT INTO matiere (nom_mat) VALUES ('Systeme d information numerique');
+INSERT INTO matiere (nom_mat) VALUES ('Informatique');
+INSERT INTO matiere (nom_mat) VALUES ('Systemes electronique');
+INSERT INTO matiere (nom_mat) VALUES ('Automatisme');
+INSERT INTO matiere (nom_mat) VALUES ('Reseau');
+INSERT INTO matiere (nom_mat) VALUES ('Automatique');
+INSERT INTO matiere (nom_mat) VALUES ('Outils logiciels');
+INSERT INTO matiere (nom_mat) VALUES ('Etudes/realisation ensembles pluritechnologiques');
+INSERT INTO matiere (nom_mat) VALUES ('Competences projet');
+
+--Matieres Specifiques aux MMI
+INSERT INTO matiere (nom_mat) VALUES ('Theories de l information et de la communication');
+INSERT INTO matiere (nom_mat) VALUES ('Ecritures dans les medias numeriques');
+INSERT INTO matiere (nom_mat) VALUES ('Esthetique et expression artistique');
+INSERT INTO matiere (nom_mat) VALUES ('Algorithmie et programmation');
+INSERT INTO matiere (nom_mat) VALUES ('Culture scientifique et traitement de l information');
+INSERT INTO matiere (nom_mat) VALUES ('Infographie');
+INSERT INTO matiere (nom_mat) VALUES ('Integration WEB');
+INSERT INTO matiere (nom_mat) VALUES ('Production audiovisuelle');
+INSERT INTO matiere (nom_mat) VALUES ('Developpement');
+INSERT INTO matiere (nom_mat) VALUES ('Services sur reseaux');
+INSERT INTO matiere (nom_mat) VALUES ('Programmation Objet');
+INSERT INTO matiere (nom_mat) VALUES ('Integration multimedia');
 INSERT INTO matiere (nom_mat) VALUES ('Langue vivante 2');
+--Bases de donnees deja dans informatiques
+
+--Matieres Specifiques aux R&T
+INSERT INTO matiere (nom_mat) VALUES ('Administration et securite des reseaux');
+INSERT INTO matiere (nom_mat) VALUES ('Architecture de l internet');
+INSERT INTO matiere (nom_mat) VALUES ('Developpement/exploitation des services reseaux');
+INSERT INTO matiere (nom_mat) VALUES ('Telecommunications fixes et mobiles');
+INSERT INTO matiere (nom_mat) VALUES ('Outils pour le signal');
+INSERT INTO matiere (nom_mat) VALUES ('Electronique/Physique pour les telecommunications');
+--informatique deja dans GEII
