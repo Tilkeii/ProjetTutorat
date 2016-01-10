4831bde0702c26042c4119aea916b6e97b5afb64
vinnvi <vin.doisneau@free.fr>
2016-01-10 14:02:54 +0100

    Correction probleme de police

--- includes/head.php
+++ includes/head.php
@@ -3,7 +3,7 @@
     <link rel="stylesheet" href="foundation/css/normalize.css"/>
     <link rel="stylesheet" href="CSS/default.css"/>
     <link rel="stylesheet" type="text/css" href="sweetalert-master/dist/sweetalert.css">
-
+	<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
     <script src="sweetalert-master/dist/sweetalert.min.js"></script>
     <script src="foundation/js/vendor/modernizr.js"></script>
 </head>
