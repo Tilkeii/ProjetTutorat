a7935a83207e48b0c5d93b6c7dd6f35102187974
root <root@debianVinvin>
2016-02-15 08:23:46 +0100

    ajout d'un tableau d'eleves dans admin

--- admin.php
+++ admin.php
@@ -18,10 +18,33 @@ if(isset($_SESSION['login']) and isset($_SESSION['pass'])) {
         <div class="off-canvas-wrap" data-offcanvas>
 
             <?php include 'includes/menu.php' ?>
-
+			
             <div class="content medium-12 large-8">
-                Page admin
-            </div>
+            	<div class="row">
+					<div class="large-8 small-12 columns"><h3>Liste des Etudiants</h3></div>
+				</div>
+				<table>
+					<thead>
+					<tr>
+						<th>Numéro étudiant</th>
+						<th>Prénom</th>
+						<th>Nom</th>
+					</tr>
+					</thead>
+					<!--récupération des étudiants et insertion dans tableau-->
+					<?php
+					$req = $bdd->prepare('select * from etudiant');
+					$req->execute() or die(print_r($bdd->errorInfo(),true));
+					$etudiants = $req->fetchAll();
+					
+					foreach($etudiants as $r)
+						echo"<tr><td>" . $r["numero_etudiant"] . "</td>" .
+								"<td>" . $r["prenom"] . "</td>" .
+								"<td>" . $r["nom"] . "</td>" ;
+					?>
+				</table>
+			</div>
+
         </div>
         <?php include('includes/footer_scripts.php'); ?>
     </body>
